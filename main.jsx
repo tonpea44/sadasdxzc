dc360f5dbaf4a03eaec0447f7b6ba12c75df755d
tonpea44 <tonpea44@gmail.com>
2025-05-17 07:58:45 +0700

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <div className="text-center text-xl mt-10 text-teal-600">ระบบแชร์สูตรอาหาร (พร้อมใช้งาน)</div>
+  </React.StrictMode>
+);
